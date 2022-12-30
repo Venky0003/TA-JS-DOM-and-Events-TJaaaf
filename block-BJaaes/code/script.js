5a5fe1921101ddd4255cc5e92a41773301fab560
Venky0003 <rajv17270@gmail.com>
2022-12-30 18:53:57 +0530

    updating

---
+++ block-BJaaes/code/script.js
@@ -0,0 +1,20 @@
+let form = document.querySelector("form");
+let errorMsg = {};
+function displayError(name){
+    form.elements[name].nextElementSibling.innerText = errorMsg[name];
+    form.elements[name].classList.add("error");
+    console.log(form.element,name)
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+
+    let elements = event.target.elements;
+
+    let username = elements.username.value;
+    if(username.length < 4){
+        errorMsg.username = "User Name can't be less than four characters";
+        displayError("username");
+    }
+}
+form.addEventListener("submit", handleSubmit);
---
+++ block-BJaaet/code/script.js
@@ -0,0 +1,66 @@
+// function addMovie(){
+//     let movieName = document.getElementById('movieName').value;
+
+//     if(movieName){
+//       let watchList = document.getElementById('watchList');
+
+//       let li = document.createElement('li');
+//       li.innerHTML = movieName;
+
+//       let deleteBtn = document.createElement('button');
+//       deleteBtn.innerHTML = 'X';
+//       deleteBtn.onclick = deleteMovie;
+
+//       li.appendChild(deleteBtn);
+//       watchList.appendChild(li);
+
+//       document.getElementById('movieName').value = '';
+//     }
+//   }
+
+//   function deleteMovie(evt){
+//     console.log(evt);
+//     let li = evt.target.parentElement;
+//     let ul = li.parentElement;
+
+//     ul.removeChild(li);
+//   }
+function deleteMovie(evt){
+    console.log(evt);
+    let li = evt.target.parentElement;
+    console.log(li)
+    let ul = li.parentElement;
+    
+    ul.removeChild(li);
+}
+let form = document.querySelector("form");
+function handleSubmit(event){
+    event.preventDefault();
+
+    let moviename = event.target.elements.movie.value;
+    console.log(moviename);
+    if(moviename){
+        let addlist = document.getElementById('watchList');
+
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+    
+
+        let li = document.createElement('li');
+        li.innerText = moviename;
+
+        // let checkbox = document.createElement('input');
+        // checkbox.type = 'checkbox';
+
+        let deleteBtn = document.createElement('span');
+        deleteBtn.classList.add("delete-Btn")
+        deleteBtn.innerText = '❌';
+        deleteBtn.onclick = deleteMovie;
+
+        
+        addlist.append(li);
+        li.append(checkbox);
+        li.append(deleteBtn);
+    }
+}
+form.addEventListener("submit",handleSubmit);
